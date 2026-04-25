3970335f45005c2b7899287a95b0eea4686d90ac
beetle-enthusiast <kburden120@gmail.com>
2026-04-25 23:45:59 +1200

    Create initial quartus project

---
+++ flappy_bird.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity flappy_bird is
+  port (
+  );
+end flappy_bird;
+
+architecture hw_interface of flappy_bird is
+
+begin
+
+end architecture;
