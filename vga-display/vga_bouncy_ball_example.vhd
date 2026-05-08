21b7727a1d5953c7e42df77727ed0f5b63e24120
Boshra Rahnamaee <brah758@aucklanduni.ac.nz>
2026-05-08 15:37:53 +1200

    Bouncy ball: top level, added pin assignment

--- vga-display/vga_bouncy_ball_example.vhd
+++ vga-display/vga_bouncy_ball_example.vhd
@@ -5,9 +5,11 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 
 ENTITY bouncy_ball_top_level IS
+	--pb_colour_1 = KEY[0] and pb_colour_2 = KEY[1], clk = CLOCK_50
+	--red_out = VGA_R[3], green_out = VGA_G[3], blue_out = VGA_B[3], h_sync_out = VGA_HS, v_sync_out = VGA_VS 
 	PORT
-		( pb_colour_1, pb_colour_2, clk, reset	: IN std_logic;
-		  red_out, green_out, blue_out, h_sync_out, v_sync_out: OUT std_logic);		
+		( KEY[0], KEY[1], CLOCK_50, reset: IN std_logic;
+		  VGA_R[3], VGA_G[3], VGA_B[3], VGA_HS, VGA_VS: OUT std_logic);		
 END bouncy_ball_top_level;
 
 architecture behavior of bouncy_ball is
@@ -37,12 +39,17 @@ SIGNAL pixel_row, pixel_column	: std_logic_vector(9 DOWNTO 0);
 SIGNAL vert_sync				: std_logic;
 SIGNAL clk_25MHz				: std_logic;
 SIGNAL red, green, blue			: std_logic;
+SIGNAL pb_colour_1, pb_colour_2	: std_logic;
+SIGNAL red_out, green_out, blue_out	: std_logic;
+SIGNAL h_sync_out, v_sync_out	: std_logic;
 
 BEGIN  
--- Instantiate VGA SYNC component
+
+reset <= '0';
+
 -- Instantiate PLL component to generate 25MHz clock from 50MHz input clock		
 PLL: PLL
-PORT MAP (refclk => clk, reset => reset, outclk0 => clk_25MHz, export => open);
+PORT MAP (refclk => CLOCK_50, reset => reset, outclk0 => clk_25MHz, export => open);
 
 -- Instantiate BOUNCY_BALL component
 BOUNCY_BALL: bouncy_ball
@@ -50,13 +57,22 @@ PORT MAP (	pb1 => pb_colour_1, pb2 => pb_colour_2, clk => clk_25MHz, vert_sync =
 			pixel_row => pixel_row, pixel_column => pixel_column,
 			red => red, green => green, blue => blue);
 
-
+-- Instantiate VGA SYNC component
 VGA_SYNC: VGA_SYNC   
 PORT MAP (	clock_25Mhz => clk_25MHz, 
 			red => red, green => green, blue => blue,
 			red_out => red_out, green_out => green_out, blue_out => blue_out, 
 			horiz_sync_out => h_sync_out, vert_sync_out => v_sync_out,
 			pixel_row => pixel_row, pixel_column => pixel_column);
+
+-- For readibility
+pb_colour_1 <= KEY[0];
+pb_colour_2 <= KEY[1];
+red_out <= VGA_R[3];
+green_out <= VGA_G[3];
+blue_out <= VGA_B[3];
+h_sync_out <= VGA_HS;
+v_sync_out <= VGA_VS;
 
 END behavior;
 
