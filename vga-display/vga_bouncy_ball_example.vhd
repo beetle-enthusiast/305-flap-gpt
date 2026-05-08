3e6ba808c30ce37ab4b1843e6944a554def7116b
Boshra Rahnamaee <brah758@aucklanduni.ac.nz>
2026-05-08 15:24:16 +1200

    Bouncy ball: instantiated components

---
+++ vga-display/vga_bouncy_ball_example.vhd
@@ -0,0 +1,62 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+
+
+ENTITY bouncy_ball_top_level IS
+	PORT
+		( pb_colour_1, pb_colour_2, clk, reset	: IN std_logic;
+		  red_out, green_out, blue_out, h_sync_out, v_sync_out: OUT std_logic);		
+END bouncy_ball_top_level;
+
+architecture behavior of bouncy_ball is
+-- VGA SYNC component signals
+COMPONENT VGA_SYNC
+	PORT(	clock_25Mhz, red, green, blue		: IN	STD_LOGIC;
+			red_out, green_out, blue_out, horiz_sync_out, vert_sync_out	: OUT	STD_LOGIC;
+			pixel_row, pixel_column: OUT STD_LOGIC_VECTOR(9 DOWNTO 0));
+END COMPONENT;
+
+--PLL component signals
+COMPONENT PLL
+	PORT(	refclk, reset: IN std_logic;
+			outclk0, export: OUT std_logic);
+END COMPONENT;
+
+--BOUNCY_BALL component signals
+COMPONENT bouncy_ball
+	PORT
+		( pb1, pb2, clk, vert_sync	: IN std_logic;
+		  pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+		  red, green, blue 			: OUT std_logic);		
+END COMPONENT;
+
+-- Signals to connect components
+SIGNAL pixel_row, pixel_column	: std_logic_vector(9 DOWNTO 0);
+SIGNAL vert_sync				: std_logic;
+SIGNAL clk_25MHz				: std_logic;
+SIGNAL red, green, blue			: std_logic;
+
+BEGIN  
+-- Instantiate VGA SYNC component
+-- Instantiate PLL component to generate 25MHz clock from 50MHz input clock		
+PLL: PLL
+PORT MAP (refclk => clk, reset => reset, outclk0 => clk_25MHz, export => open);
+
+-- Instantiate BOUNCY_BALL component
+BOUNCY_BALL: bouncy_ball
+PORT MAP (	pb1 => pb_colour_1, pb2 => pb_colour_2, clk => clk_25MHz, vert_sync => vert_sync,
+			pixel_row => pixel_row, pixel_column => pixel_column,
+			red => red, green => green, blue => blue);
+
+
+VGA_SYNC: VGA_SYNC   
+PORT MAP (	clock_25Mhz => clk_25MHz, 
+			red => red, green => green, blue => blue,
+			red_out => red_out, green_out => green_out, blue_out => blue_out, 
+			horiz_sync_out => h_sync_out, vert_sync_out => v_sync_out,
+			pixel_row => pixel_row, pixel_column => pixel_column);
+
+END behavior;
+
