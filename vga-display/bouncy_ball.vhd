ecc364f2543b3020724ebe06e4cb2b7488f7e8f9
Boshra Rahnamaee <brah758@aucklanduni.ac.nz>
2026-05-29 10:31:49 +1200

    updated bird code

--- vga-display/bouncy_ball.vhd
+++ vga-display/bouncy_ball.vhd
@@ -1,25 +1,27 @@
 LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.all;
-use ieee.numeric_std.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
+ENTITY bird IS
+    PORT (
+        enable, click, clk, vert_sync : IN std_logic;
+        pixel_row, pixel_column       : IN std_logic_vector(9 DOWNTO 0);
+        bird_on_out                   : OUT std_logic;
+        red, green, blue              : OUT std_logic_vector(3 DOWNTO 0);
+        bird_y_pos, size              : OUT std_logic_vector(9 DOWNTO 0);
+        bird_x_pos                    : OUT std_logic_vector(10 DOWNTO 0)
+    );
+END bird;
 
-ENTITY bouncy_ball IS
-	PORT
-		( enable, click, clk, vert_sync	: IN std_logic;
-          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  ball_on_out: OUT std_logic;
-		  red, green, blue : OUT std_logic_vector (3 DOWNTO 0);
-		  ball_y_pos, size: OUT std_logic_vector(9 DOWNTO 0);
-		  ball_x_pos: OUT std_logic_vector(10 DOWNTO 0));		
-END bouncy_ball;
+ARCHITECTURE behavior OF bird IS
 
-architecture behavior of bouncy_ball is
+SIGNAL bird_on: std_logic := '0';
+SIGNAL prev_click              : std_logic := '0';
+SIGNAL start: std_logic:= '0';
+SIGNAL size_temp        : unsigned(9 DOWNTO 0)  := to_unsigned(8, 10);          -- radius = 8
+SIGNAL bird_y_pos_temp  : unsigned(9 DOWNTO 0)  := to_unsigned(240 - 8, 10);    -- start near bottom
+SIGNAL bird_x_pos_temp  : unsigned(10 DOWNTO 0) := to_unsigned(200, 11);        -- fixed X
 
-SIGNAL ball_on, ball_white: std_logic;
-SIGNAL prev_click, ball_collision: std_logic := '0';
-SIGNAL size_temp : std_logic_vector(9 DOWNTO 0) := std_logic_vector(to_unsigned(16, 10));
-SIGNAL ball_y_pos_temp : std_logic_vector(9 DOWNTO 0) := std_logic_vector(to_unsigned(479-8, 10));
-SIGNAL ball_x_pos_temp : std_logic_vector(10 DOWNTO 0) := std_logic_vector(to_unsigned(400, 11));
 -- ROM SIGNALS
 SIGNAL rom_data : std_logic_vector(11 downto 0);
 SIGNAL rom_address : std_logic_vector(11 downto 0);
@@ -32,7 +34,17 @@ SIGNAL vs_prev : std_logic := '0';
 SIGNAL enable_reg : std_logic := '0';
 
 
-BEGIN           
+BEGIN       
+
+
+ -- bird coverage
+bird_on <= '1' WHEN
+    (unsigned(bird_x_pos_temp) <= unsigned(pixel_column) + size_temp AND
+        unsigned(pixel_column)    <= unsigned(bird_x_pos_temp) + size_temp AND
+        unsigned(bird_y_pos_temp) <= unsigned(pixel_row) + size_temp AND
+        unsigned(pixel_row)       <= unsigned(bird_y_pos_temp) + size_temp)
+    ELSE '0';
+
 
 -- Rom component
 SPRTIE_ROM : entity work.bird
@@ -45,17 +57,17 @@ SPRTIE_ROM : entity work.bird
 Sprite : process(clk) 
 begin 
 	if rising_edge(clk) then 
-		if (unsigned(pixel_row) >= unsigned(ball_y_pos_temp) - 16 and
-    unsigned(pixel_row) <  unsigned(ball_y_pos_temp) + 16 and
-    unsigned(pixel_column) >= unsigned(ball_x_pos_temp(9 downto 0)) - 16 and
-    unsigned(pixel_column) <  unsigned(ball_x_pos_temp(9 downto 0)) + 16) then 
+		if (unsigned(pixel_row) >= unsigned(bird_y_pos_temp) - 16 and
+    unsigned(pixel_row) <  unsigned(bird_y_pos_temp) + 16 and
+    unsigned(pixel_column) >= unsigned(bird_x_pos_temp(9 downto 0)) - 16 and
+    unsigned(pixel_column) <  unsigned(bird_x_pos_temp(9 downto 0)) + 16) then 
 		in_sprite_bounds <= '1';
 	else 
 		in_sprite_bounds <= '0';
 	end if;
 
-	row_offset <= std_logic_vector(unsigned(pixel_row) - (unsigned(ball_y_pos_temp) - 16));
-	col_offset <= std_logic_vector(unsigned(pixel_column) - (unsigned(ball_x_pos_temp(9 downto 0)) - 16));
+	row_offset <= std_logic_vector(unsigned(pixel_row) - (unsigned(bird_y_pos_temp) - 16));
+	col_offset <= std_logic_vector(unsigned(pixel_column) - (unsigned(bird_x_pos_temp(9 downto 0)) - 16));
 
  		if (in_sprite_bounds = '1') then 
 			rom_address <= (row_offset(4 downto 0) & "0") & (col_offset(4 downto 0) & "0");
@@ -83,62 +95,80 @@ begin
 		blue <= "0000";
 	end if;
 
-	ball_on <= sprite_on;
-	ball_on_out <= ball_on;
+	bird_on <= sprite_on;
+	bird_on_out <= bird_on;
 end if;
 end process;
 
 
-Move_Ball: process (clk) 
-VARIABLE ball_y_motion : signed(9 DOWNTO 0) := (others => '0');
-VARIABLE at_top : std_logic := '0';
-begin
-    if rising_edge(clk) then
-        enable_reg <= enable;
-        vs_prev    <= vert_sync;
+-- Movement
+    Move_bird : PROCESS(vert_sync)
+        VARIABLE bird_y_motion : signed(9 DOWNTO 0) := (OTHERS => '0'); -- Velocity of bird
+		  VARIABLE at_top        : std_logic := '0';
+        VARIABLE next_y        : signed(9 DOWNTO 0); -- Next y position of bird
+        CONSTANT bottom_limit  : unsigned(9 DOWNTO 0) := to_unsigned(479, 10);
+		  CONSTANT top_limit  : unsigned(9 DOWNTO 0) := to_unsigned(55, 10);
 
-        if vs_prev = '1' and vert_sync = '0' and enable_reg = '1' then
+		  
+    BEGIN
+        IF (rising_edge(vert_sync) AND enable = '1') THEN
+		  
+			-- Game not started and user clicks on screen to start
+			IF (start = '0' AND click = '1' AND prev_click = '0') THEN
+				start <= '1';
+	
+			-- Begin the game
+			ELSIF (start = '1') THEN
+            -- Bottom hit
+            IF unsigned(bird_y_pos_temp) >= (bottom_limit - size_temp) THEN
+                bird_y_motion := (OTHERS => '0');
+                at_top        := '0';
 
-            if signed(ball_y_pos_temp) >= to_signed(479,10) - signed(size_temp) then
-                ball_y_motion := (others => '0');
-                at_top := '0';
+            -- Top hit
+            ELSIF unsigned(bird_y_pos_temp) <= (top_limit- size_temp) THEN
+                at_top        := '1';
+                bird_y_motion := bird_y_motion + to_signed(1, 10);  -- gravity
 
-            elsif signed(ball_y_pos_temp) <= signed(size_temp) then
-                at_top := '1';
-                ball_collision <= not ball_collision;
-                ball_y_motion := ball_y_motion + 1;
+            ELSE
+                at_top        := '0';
+                bird_y_motion := bird_y_motion + to_signed(1, 10);  -- gravity
+            END IF;
+				
 
-            else
-                at_top := '0';
-                ball_y_motion := ball_y_motion + 1;
-            end if;
+            -- Click = jump
+            IF (click = '1' AND prev_click = '0' AND at_top = '0') THEN
+                bird_y_motion := -to_signed(8, 10);  -- jump up
+                prev_click    <= '1';
+            ELSE
+                prev_click    <= click;
+            END IF;
+				
 
-            if click = '1' and prev_click = '0' and at_top = '0' then
-                ball_y_motion := to_signed(-8, 10);
-                prev_click <= '1';
-            else
-                prev_click <= click;
-            end if;
+            -- Compute next position
+            next_y := signed(bird_y_pos_temp) + bird_y_motion;
 
-            ball_y_pos_temp <= std_logic_vector(signed(ball_y_pos_temp) + ball_y_motion);
+				
+            -- Clamp to top
+            IF next_y <= signed(size_temp) THEN
+                next_y := signed(size_temp);
+            END IF;
+				
 
-            if signed(ball_y_pos_temp) + ball_y_motion <= signed(size_temp) then
-                ball_y_pos_temp <= size_temp;
-            end if;
+            -- Clamp to bottom
+            IF next_y >= signed(bottom_limit - size_temp) THEN
+                next_y := signed(bottom_limit - size_temp);
+            END IF;
 
-            if signed(ball_y_pos_temp) + ball_y_motion >= to_signed(479,10) - signed(size_temp) then
-                ball_y_pos_temp <= std_logic_vector(to_signed(479,10) - signed(size_temp));
-            end if;
+            bird_y_pos_temp <= unsigned(next_y);
+				
+			END IF;
 
-        end if;
-    end if;
-end process Move_Ball;
+        END IF;
+    END PROCESS Move_bird;
 
-
---Output signal assignments
-ball_y_pos <= ball_y_pos_temp;
-ball_x_pos <= ball_x_pos_temp;
-size <= size_temp;
-
+    -- Outputs
+    bird_y_pos <= std_logic_vector(bird_y_pos_temp);
+    size       <= std_logic_vector(size_temp);
+    bird_x_pos <= std_logic_vector(bird_x_pos_temp);
 
 END behavior;
